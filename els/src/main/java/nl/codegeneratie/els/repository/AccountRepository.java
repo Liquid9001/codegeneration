56e0b13c8e6802c498fbd6504edb16427d3174e3
LawrenceDeveloper <113107075+LawrenceDeveloper@users.noreply.github.com>
2026-05-20 01:57:28 +0200

    Update AccountRepository.java

--- els/src/main/java/nl/codegeneratie/els/repository/AccountRepository.java
+++ els/src/main/java/nl/codegeneratie/els/repository/AccountRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-	List<Account> findByUser(User user);
+	Optional<Account> findByUser(User user);
 	Optional<Account> findByIban(String iban);
 }
 
